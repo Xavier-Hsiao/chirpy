30d29e738d67e98cca517e0123cba86aa1f9d185
Xavier <tim88776@gmail.com>
2024-10-23 22:52:12 +0800

    feat: initialize basic HTTP server with 404 responses

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	const port = "8080"
+	mux := http.NewServeMux()
+	server := &http.Server{
+		Handler: mux,
+		Addr:    ":" + port,
+	}
+
+	log.Printf("Serving on port: %s", port)
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal("Failed to open the server!\n")
+	}
+}

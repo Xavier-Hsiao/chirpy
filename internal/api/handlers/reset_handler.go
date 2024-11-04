e4d4690051125082c8fe1e46efe82e130920e008
Xavier <tim88776@gmail.com>
2024-11-04 22:26:44 +0800

    docs: add API documentation for /admin/reset endpoint

--- internal/api/handlers/reset_handler.go
+++ internal/api/handlers/reset_handler.go
@@ -7,6 +7,14 @@ import (
 	"github.com/Xavier-Hsiao/Chirpy/internal/config"
 )
 
+// @Summary		Reset Metrics
+// @Description	Reset the number of times Chirpy has been visisted to 0
+// @Tags			metrics
+// @ID				post-reset
+// @Produce		plain
+// @Success		200	{string}	string	"Hits set to 0"
+// @Example		200 "<html><body><h1>Welcome, Chirpy Admin</h1><p>Chirpy has been visited 42 times!</p></body></html>"
+// @Router			/admin/reset [post]
 func HandlerReset(cfg *config.ApiConfig) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		cfg.FileServerHits.Store(0)

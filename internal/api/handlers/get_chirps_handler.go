8a8153efc4010325ca864c71b5bf5afea9c97445
Xavier <tim88776@gmail.com>
2024-12-02 21:38:17 +0800

    feat: add author filter to GET /api/chirps endpoint

--- internal/api/handlers/get_chirps_handler.go
+++ internal/api/handlers/get_chirps_handler.go
@@ -6,29 +6,55 @@ import (
 
 	"github.com/Xavier-Hsiao/Chirpy/internal/config"
 	"github.com/Xavier-Hsiao/Chirpy/internal/helpers"
+	"github.com/Xavier-Hsiao/Chirpy/internal/models"
+	"github.com/google/uuid"
 )
 
-//	@Summary		Get all chirps
-//	@Description	Retreive all chirps from database in ascendent order of created_at time
-//	@Tags			chirp
-//	@ID				get-chirps
-//	@Produce		json
-//	@Success		200	{array}		models.Chirp			"created chirp's information"
-//	@Failure		500	{object}	helpers.ErrorResponse	"Internal server error: can not deal with data properly"
-//	@Router			/api/chirps [get]
+// @Summary		Get all chirps
+// @Description	Retreive all chirps from database in ascendent order of created_at time
+// @Tags			chirp
+// @ID				get-chirps
+// @Produce		json
+// @Success		200	{array}		models.Chirp			"created chirp's information"
+// @Failure		500	{object}	helpers.ErrorResponse	"Internal server error: can not deal with data properly"
+// @Router			/api/chirps [get]
 func HandlerGetChirps(cfg *config.ApiConfig) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		//  Get the slice of Chirps from database
-		chirpsDB, err := cfg.DBQueries.GetChirps(r.Context())
+		dbChirps, err := cfg.DBQueries.GetChirps(r.Context())
 		if err != nil {
 			helpers.RespondWithError(w, http.StatusInternalServerError, "Failed to get chirps from database", err)
 			return
 		}
-		chirps := helpers.ConvertChirps(chirpsDB)
+
+		authorIDString := r.URL.Query().Get("author_id")
+		authorID := uuid.Nil
+		if authorIDString != "" {
+			authorID, err = uuid.Parse(authorIDString)
+			if err != nil {
+				helpers.RespondWithError(w, http.StatusBadRequest, "Invalid author ID", err)
+				return
+			}
+		}
+
+		chirps := []models.Chirp{}
+		for _, dbChirp := range dbChirps {
+			if authorID != uuid.Nil && dbChirp.UserID != authorID {
+				continue
+			}
+
+			chirps = append(chirps, models.Chirp{
+				ID:        dbChirp.ID,
+				CreatedAt: dbChirp.CreatedAt,
+				UpdatedAt: dbChirp.UpdatedAt,
+				UserID:    dbChirp.UserID,
+				Body:      dbChirp.Body,
+			})
+		}
 
 		// Respond with json data
 		helpers.RespondWithJson(w, http.StatusOK, chirps)
 
-		log.Println("Retreive all chirps!")
+		log.Println("Retreive required chirps!")
 	}
 }
